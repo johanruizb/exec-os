b6fd7058a5f950bb62086184ac9f032aada6124f
johanruizb <62685623+johanruizb@users.noreply.github.com>
2021-12-19 11:00:54 -0500

    Update leercadena_main.c
    Se añaden comentarios al codigo

--- leercadena_main.c
+++ leercadena_main.c
@@ -1,3 +1,15 @@
+/*
+ * Durante la clase se llevó a cabo la construcción de un shell muy 
+ * básico el cual permitía ejecutar comandos de manera interactiva. 
+ * Se han hecho los ajustes necesarios en el código leercadena_main.c
+ * para tener un comportamiento deseado.
+ * 
+ * Codigo fuente: https://github.com/josanabr/so_80/tree/master/05/dummy_shell
+ * 
+ * Modificado por: Johan Ruiz - johan.andres.ruiz@correounivalle.edu.co
+ * Fecha: 19/12/2021
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,12 +18,22 @@
 #include <unistd.h>
 #include "leercadena.h"
 
-char* vector[BUFSIZ];
+/**
+ * Recibe comandos y parametros como argumentos al ejecutarse el programa.
+ * Uso: leercadena_main <comando>
+        leercadena_main <comando> <-parametros>
+ * 
+ * @param[in] argc cantidad de argumentos
+ * @param[in] argv vector que contiene los argumentos
+ * @param[out] int retorna 0, en caso de una ejecucion correcta
+*/
 
 int main(int argc, char *argv[]) {
+char* vector[BUFSIZ];
 
   if (argc < 2) {
-    printf("Uso:\t> leercadena_main <comando>\n\t> leercadena_main <comando> <-parametros>\n");
+    printf("Uso:\t> leercadena_johan <comando>\n
+    \t> leercadena_johan <comando> <-parametros>\n");
     return 0;
   }
 
@@ -20,8 +42,9 @@ int main(int argc, char *argv[]) {
      return 0;
   }
 
+  // Recortar y guardar 'argv' en 'vector'
   for (int i = 1; i < argc; i++) {
-       vector[i - 1] = argv[i];
+    vector[i - 1] = argv[i];
   }
 
   pid_t pid = fork();
@@ -32,7 +55,7 @@ int main(int argc, char *argv[]) {
   } else if (pid == 0) {
     printf("> Comando: %s\n", vector[0]);
     execvp(vector[0], vector);
-    perror ("Error inesperado de execvp()");
+    perror("Error inesperado de execvp()");
     exit(EXIT_FAILURE);
   } else {
     wait(NULL);
@@ -40,4 +63,3 @@ int main(int argc, char *argv[]) {
 
   return 0;
 }
-
